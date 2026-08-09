13a84bf8f08bf1bc68036da6c1cb7ca3fc9ba2cb
kmkaranmaurya767 <kmkaranmaurya767@gmail.com>
2026-08-09 18:59:22 +0530

    full crude working todo list done

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const userList = document.querySelector('.user-list')
 
 const userForm = document.querySelector('form')
 
+const button = document.querySelector('#button')
+
+let editUserId = null;
 
 
 const userData = [
@@ -23,8 +26,41 @@ const userData = [
     }
 ]
 
+
+
+userList.addEventListener("click", (event) => {
+  // EDIT
+    if (event.target.classList.contains("edit-btn")) {
+
+        const id = Number(event.target.dataset.id);
+
+        const user = userData.find((elem) => elem.id === id);
+
+        editUserId = id;
+
+        document.querySelector("#name").value = user.name;
+        document.querySelector("#email").value = user.email;
+        document.querySelector("#age").value = user.age;
+    }
+
+
+    // DELETE
+    if (event.target.classList.contains("delete-btn")) {
+
+        const id = Number(event.target.dataset.id);
+
+        const userIndex = userData.findIndex((elem) => elem.id === id);
+
+        userData.splice(userIndex, 1);
+
+        renderUser();
+    }
+});
+
 function renderUser(){
     userList.innerHTML = "";
+
+
     userData.forEach((elem) => {
     userList.innerHTML += `<div class="card">
             <div class="box">
@@ -35,8 +71,8 @@ function renderUser(){
                 <p ><b>Email:</b> ${elem.email} </p>
                 <p><b>Age:</b> ${elem.age} </p>
                 <div class="action">
-                    <button class="delete-btn"> 🗑️</button>
-                    <button class="edit-btn">✏️</button>
+                <button class="delete-btn" data-id="${elem.id}">🗑️</button>
+                <button class="edit-btn" data-id="${elem.id}">✏️</button>
                 </div>      
             </div>
         </div>`
@@ -45,30 +81,62 @@ function renderUser(){
 renderUser()
 
 userForm.addEventListener('submit', (event) => {
+    event.preventDefault();
     const name = document.querySelector('#name').value
     const  email= document.querySelector('#email').value
     const age = document.querySelector('#age').value
     const image = document.querySelector('#image').files[0]
 
 
-    const user =  {
-        name,
-        email,
-        age,
-        image: image ? URL.createObjectURL(image) : null
-    }
+    if (editUserId !== null) {
 
-    if (!name || !email || !age) {
+        //UPDATE
+
+        const user = userData.find((elem) => elem.id === editUserId);
+
+        user.name = name;
+        user.email = email;
+        user.age = age;
+
+        if (image) {
+            user.image = URL.createObjectURL(image);
+        }
+
+        editUserId = null;
+
+        
+    }
+    else{
+          // ADD NEW USER
+
+        const user = {
+            id: Date.now(),
+            name: name,
+            email: email,
+            age: age,
+            image: image
+                ? URL.createObjectURL(image)
+                : null
+        };
+
+         if (!name || !email || !age) {
         alert("Please fill all fields")
         return
     }
     else{
         userData.push   (user)
         console.log(userData)
-        userForm.reset()
-        renderUser()
+       
     }
+    }
+  
+     userForm.reset()
+        renderUser()
+
+   
 })
+
+
 
 
 
